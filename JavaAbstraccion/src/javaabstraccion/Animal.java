ed2e33a30588ef63e5028890bdbdda074935ce81
OscarGonzalezADSI <55475821+OscarGonzalezADSI@users.noreply.github.com>
2020-08-28 23:55:55 -0500

    Add files via upload

---
+++ JavaAbstraccion/src/javaabstraccion/Animal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaabstraccion;
+
+/**
+ *
+ * @author Usuario
+ */
+// Abstract class
+abstract class Animal {
+  // Abstract method (does not have a body)
+  public abstract void animalSound();
+  // Regular method
+  public void sleep() {
+    System.out.println("Zzz");
+  }
+}
