b09ac7399e01925a839a028b1f3a8f8167e9e92f
OscarGonzalezADSI <55475821+OscarGonzalezADSI@users.noreply.github.com>
2020-08-28 23:46:41 -0500

    Add files via upload

---
+++ JavaExcepciones/src/javaexcepciones/ExcepcionPersonalizada.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaexcepciones;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ExcepcionPersonalizada {
+    static void Edad(int age){
+        if (age < 18) {
+            throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+        }
+        else {
+            System.out.println("Access granted - You are old enough!");
+        }        
+    }
+}
---
+++ JavaExcepciones/src/javaexcepciones/JavaExcepciones.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaexcepciones;
+
+/**
+ *
+ * @author Usuario
+ */
+public class JavaExcepciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            int[] myNumbers = {1, 2, 3};
+            System.out.println(myNumbers[10]);
+        } catch (Exception e) {
+            System.out.println("Something went wrong.");
+        } finally {
+            System.out.println("The 'try catch' is finished.");
+        }
+        
+        // ---------------------------------------------------------------------
+        // Excepcion personalizada
+        // ---------------------------------------------------------------------
+        try {
+            ExcepcionPersonalizada.Edad(66);
+        } catch (Exception e) {
+            System.out.println("Something went wrong.");
+        } finally {
+            System.out.println("The 'try catch' is finished.");
+        }
+    }
+}
