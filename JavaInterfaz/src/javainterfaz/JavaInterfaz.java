3988f39bbb74966b0cb81c3505d0f95b0b3dae36
OscarGonzalezADSI <55475821+OscarGonzalezADSI@users.noreply.github.com>
2020-08-28 23:54:22 -0500

    Add files via upload

---
+++ JavaInterfaz/src/javainterfaz/JavaInterfaz.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javainterfaz;
+
+/**
+ *
+ * @author Usuario
+ */
+public class JavaInterfaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      DemoClass myObj = new DemoClass();
+      myObj.myMethod();
+      myObj.myOtherMethod();
+    }
+}
---
+++ JavaInterfaz/src/javainterfaz/SecondInterface.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javainterfaz;
+
+/**
+ *
+ * @author Usuario
+ */
+interface SecondInterface {
+  public void myOtherMethod(); // interface method
+}
